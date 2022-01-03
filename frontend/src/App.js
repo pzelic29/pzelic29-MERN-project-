289a3f332ecff482e0d7e619ab758ec0de682e22
pzelic29 <pzelic29@gmail.com>
2022-01-03 17:43:41 +0100

    Adding a UsersList and UserItem Page/Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
-import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
-import Users  from "./users/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-function App() {
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
   return (
-  <Router>
-    <Switch>
-    <Route path="/" exact>
-      <Users/>
-    </Route>
-    <Route path="/places/new" exact>
-      <NewPlace/>
-    </Route>
-    <Redirect to="/" />
-    </Switch>
-  </Router>
-  
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 };
 
--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from '../components/UsersList';
 
-const Users = () =>{
-    return <h2>Users</h2>;
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Petra',
+      image:
+        '',
+      places: 2
+    }
+  ];
 
+  return <UsersList items={USERS} />;
 };
+
 export default Users;
