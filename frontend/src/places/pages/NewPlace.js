2459949fb01ba454279e0ed767b82ae01fe145ef
pzelic29 <pzelic29@gmail.com>
2022-01-04 14:58:44 +0100

    Adding a Form Input component and css file NewPlace.css

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+
+import Input from "../../shared/FormElements/Input";
+import './NewPlace.css';
+
 const NewPlace =()=>{
-    return <h2>New Place</h2>
+    return <form className="place-form">
+        <Input elemenz="input" type="text" label="Title" />
+    </form>;
 };
 
 export default NewPlace;
