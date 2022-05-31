aaa92d7ff6723cf323ab644100e49aef24294281
pzelic29 <pzelic29@gmail.com>
2022-05-31 13:56:06 +0200

    update and delete place

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
-import {useHttpClient} from '../../shared/hooks/http-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const UserPlaces = () => {
-  const [loadedPlaces ,setLoadedPlace] =useState();
-  const {isLoading, error, sendRequest, clearError} =useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const userId = useParams().userId;
 
@@ -18,13 +18,17 @@ const UserPlaces = () => {
         const responseData = await sendRequest(
           `http://localhost:3001/api/places/user/${userId}`
         );
-        setLoadedPlace(responseData.places);
-      } catch (err) {
-        
-      }
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
     };
     fetchPlaces();
-  },[sendRequest, userId]);
+  }, [sendRequest, userId]);
+
+  const placeDeletedHandler = deletedPlaceId => {
+    setLoadedPlaces(prevPlaces =>
+      prevPlaces.filter(place => place.id !== deletedPlaceId)
+    );
+  };
 
   return (
     <React.Fragment>
@@ -34,10 +38,11 @@ const UserPlaces = () => {
           <LoadingSpinner />
         </div>
       )}
-      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+      {!isLoading && loadedPlaces && (
+        <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />
+      )}
     </React.Fragment>
   );
 };
 
 export default UserPlaces;
-
